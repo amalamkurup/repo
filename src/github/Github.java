d25bdc81e0412aab94997a69b5819193120d1027
amala <amala.mk94@gmail.com>
2023-08-07 15:26:57 +0800

    first commit

--- src/github/Github.java
+++ src/github/Github.java
@@ -1,5 +1,27 @@
 package github;
 
 public class Github {
+	
+public static void main(String[] args) {
+		
+		int a=60;
+		int b=20;
+		int c=a+b;
+		int d=a-b;
+		System.out.println("Arithemetic Operators");
+		System.out.println("--------");
+		System.out.println(c);
+		System.out.println("Addition:"+c);
+		System.out.println("Substraction:"+d);
+		System.out.println("multiplication:"+a*b);
+		System.out.println("division:"+a/b);
+		System.out.println(a%b);
+
+
+	}
 
 }
+
+	
+
+
